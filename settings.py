18f1b3946bdf37c9710d354995d1e1b45bc04208
Iosif Mazan <josephmazan6174@gmail.com>
2025-08-03 18:19:15 +0300

    refactor: update ci

--- settings.py
+++ settings.py
@@ -1,7 +1,5 @@
-from pathlib import Path
-
 from pydantic_settings import BaseSettings, SettingsConfigDict
-
+from pydantic import Field
 
 class Settings(BaseSettings):
     model_config = SettingsConfigDict(
@@ -9,25 +7,25 @@ class Settings(BaseSettings):
     )
 
     # Database settings
-    DATABASE_HOST: str
-    DATABASE_PORT: int = 5432
-    DATABASE_USER: str
-    DATABASE_PASSWORD: str
-    DATABASE_NAME: str
+    DB_HOST: str = Field(default=..., description="DB Host")
+    DB_PORT: int = Field(default=5432, description="DB Port")
+    DB_USER: str = Field(default=..., description="DB User")
+    DB_PASSWORD: str = Field(default=..., description="DB Password")
+    DB_NAME: str = Field(default=..., description="DB Name")
 
     # API settings
-    API_KEY: str
-    API_KEY_NAME: str = "X-API-KEY"
+    API_KEY: str = Field(default=..., description="API Key")
+    API_KEY_NAME: str = Field(default="X-API-KEY", description="API Key Name")
 
     @property
     def database_url(self) -> str:
         return (
             f"postgresql+asyncpg://"
-            f"{self.DATABASE_USER}:"
-            f"{self.DATABASE_PASSWORD}@"
-            f"{self.DATABASE_HOST}:"
-            f"{self.DATABASE_PORT}/"
-            f"{self.DATABASE_NAME}"
+            f"{self.DB_USER}:"
+            f"{self.DB_PASSWORD}@"
+            f"{self.DB_HOST}:"
+            f"{self.DB_PORT}/"
+            f"{self.DB_NAME}"
         )
 
 
